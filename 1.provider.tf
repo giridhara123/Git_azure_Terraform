70d0a6999ee2831c3adb056a4f761c529b4651c9
giridhara123 <giridharavenkat123@gmail.com>
2025-03-04 23:13:36 -0500

    updated tags for rg1

--- 1.provider.tf
+++ 1.provider.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.21.1"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.89.0"
     }
   }
@@ -14,9 +14,10 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features {
-    
+    # we caan add azure credentials
+
   }
 }
-provider "aws"{
+provider "aws" {
 
 }
--- 2.resource_group.tf
+++ 2.resource_group.tf
@@ -1,5 +1,8 @@
 resource "azurerm_resource_group" "rg1" {
-    name = "rg1"
-    location = "eastus"
-  
+  name     = "rg1"
+  location = "eastus"
+  tags ={
+    Environment =" Development"
+  }
+
 }
