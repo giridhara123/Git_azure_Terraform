9e32d0e80a7ea4e7ac2467fbabb9db9ddfbefd4c
giridhara123 <giridharavenkat123@gmail.com>
2025-03-04 23:38:52 -0500

    added vnets

--- 2.resource_group.tf
+++ 2.resource_group.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "rg1" {
   name     = "rg1"
   location = "eastus"
-  tags ={
-    Environment =" Development"
+  tags = {
+    Environment = " Development"
   }
 
 }
