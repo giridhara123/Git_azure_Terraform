10b4ade7ecf7bd46964f3c0629ffcda2007f9d64
giridhara123 <giridharavenkat123@gmail.com>
2025-03-06 19:18:54 -0500

    update inversion version range:

--- 3.virtual_network.tf
+++ 3.virtual_network.tf
@@ -9,5 +9,5 @@ resource "azurerm_virtual_network" "vnet2" {
   name                = "${azurerm_resource_group.rg1.name}-vnet2"
   resource_group_name = azurerm_resource_group.rg1.name #it is implicit dependecies
   location            = "westus"
-  address_space       = ["10.45.0.0/16"]
+  address_space       = ["10.46.0.0/16"]
 }
